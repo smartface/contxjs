059cb598ed591c0cf9bafff60bf5ba89c0ee8960
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2018-01-11 07:37:24 +0000

    add indexes

---
+++ src/smartface/index.js
@@ -0,0 +1,6 @@
+export const addChild = require("./action/addChild");
+export const removeChild = require("./action/removeChild");
+export const removeChildren = require("./action/removeChildren");
+export const pageContext = require("./action/pageContext");
+export const pageContextPatch = require("./action/pageContextPatch");
+export const componentContextPatch = require("./action/componentContextPatch");
---
+++ src/styling/index.js
@@ -0,0 +1,7 @@
+export const addThemeableContext = require("action/addThemeableContext");
+export const changeTheme         = require("action/changeTheme");
+export const pushClassNames      = require("action/pushClassNames");
+export const removeClassName     = require("action/removeClassName");
+export const updateContextTree   = require("action/updateContextTree");
+export const ThemeContext        = require("ThemeContext");
+export const StyleContext        = require("StyleContext");
