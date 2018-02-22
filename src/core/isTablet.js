f3b943db303a05a58c7a43a275aa3860ae6b886b
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2018-02-22 15:11:51 +0000

    Add dynamic import

--- src/core/isTablet.js
+++ src/core/isTablet.js
@@ -1,12 +1,19 @@
 import { orientation } from "sf-extension-utils";
 import System from 'sf-core/device/system';
-
+import('sf-core/util/Android/androidconfig')
+    .then((config) => {
+       AndroidConfig = config
+    });
+let AndroidConfig
 var isTablet = false;
 
 if (System.OS === "iOS" && orientation.shortEdge >= 720) {
     isTablet = true;
 }
 else if (System.OS === "Android") {
+    const Activity = AndroidConfig.activity;
+    const context = Activity;
+    
     const SCREENLAYOUT_SIZE_MASK = 15,
         SCREENLAYOUT_SIZE_LARGE = 3;
     let xlarge = Boolean((context.getResources().getConfiguration().screenLayout & SCREENLAYOUT_SIZE_MASK) === 4);
