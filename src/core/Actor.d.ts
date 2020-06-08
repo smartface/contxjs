77ef5c2e454259f6dd7447400d7dccfa958ffa5a
Furkan Arabacı <furkanarabaci@hotmail.com>
2020-06-08 16:30:07 +0300

    feature(typescript): core type definitions except context itself

---
+++ src/core/Actor.d.ts
@@ -0,0 +1,47 @@
+/**
+ * @module core/Actor
+ */
+
+
+/**
+ * Abstract Actor Class
+ */
+export default abstract class Actor {
+  /**
+   * @constructor
+   * @param {object} component - Wrapped Component
+   */
+  constructor(component: object, name: string, id: string);
+
+  updateComponent(comp: object): void;
+
+  getName(): string;
+
+  setID(id: string): void;
+
+  setName(name: string): void;
+
+  getID(): string
+
+  getInstanceID(): string
+
+  onError(err: any): any; // onError - boolean
+
+  getComponent(): object;
+
+  componentDidLeave(): void;
+
+  reset(): void;
+
+  setDirty(value: boolean): void;
+
+  getDirty(): boolean;
+
+  isChildof(parent: object): boolean;
+
+  onRemove(): void;
+
+  dispose(): void;
+
+  componentDidEnter(dispatcher: any): void;
+}
---
+++ src/core/hooks.d.ts
@@ -0,0 +1,2 @@
+
+export default function hooks(hooksList: () => {}): () => {}
